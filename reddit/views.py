ba2fae1d3cc4004ff5dcf3a1f94f8d92e0ec40eb
ZephonMakale <mzephon@gmail.com>
2021-01-21 16:00:43 +0300

    Add a view to add comments to the posts

--- reddit/views.py
+++ reddit/views.py
@@ -44,3 +44,19 @@ def post_detail(request, pk):
 def sub_detail(request, pk):
     sub = get_object_or_404(SubReddit, pk=pk)
     return render(request, 'reddit/sub_detail.html', {'sub': sub})
+
+@login_required
+def add_comment(request, pk, parent_pk=None):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.parent_id = parent_pk
+            comment.save()
+        return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'reddit/add_comment.html', {'form': form})
