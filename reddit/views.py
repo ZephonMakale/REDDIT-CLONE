e27c601bda89a531eab0f16bdaae090fa66eb986
ZephonMakale <mzephon@gmail.com>
2021-01-21 15:57:11 +0300

    Add a view to edit posts

--- reddit/views.py
+++ reddit/views.py
@@ -23,3 +23,16 @@ def post_new(request):
         form = PostForm()
     return render(request, 'reddit/post_edit.html', {'form': form, 'is_create': True})
 
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post, initial={'subreddits' : post.subreddits.all()})
+    return render(request, 'reddit/post_edit.html', {'form': form, 'is_create': False})
